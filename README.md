ca3e0fe9811c2915a5cfc120aee3b4b23e07a408
lluciocc <llucio.cc00@gmail.com>
2025-12-15 19:12:30 +0100

    Adding scripts

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # mydots - @Lluciocc
 
-## Screenshot5s
+## Screenshots
 ![screenshot1](https://github.com/Lluciocc/mydots/blob/master/screenshot/1.png)
 ![screenshot2](https://github.com/Lluciocc/mydots/blob/master/screenshot/2.png)
 ![screenshot3](https://github.com/Lluciocc/mydots/blob/master/screenshot/3.png)
@@ -15,5 +15,10 @@ If you want to get my wallpapers:
 To make the script in `hypr/wallpaper.sh` works great, you can edit the bind 
 bind = $mainMod, J, exec, ~/.local/bin/wallpaper-picker.sh
 
-# Music with waybar
+## DIMG
+Simple script to download image an place it to the right folder
+Made to work with my waybar wallpaper manager :)
+Usage: `dimg <url> [filename]`
+
+## Music with waybar
 You must use spotify, look at the script at `waybar/music.sh` for code
---
+++ scripts/dimg.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# Simple script to download image an place it to the right folder
+# Made to work with my waybar wallpaper manager :)
+# Usage: dimg <url> [filename]
+
+if [[ -z "$1" ]]; then
+    echo "Error: missing URL"
+    echo "Usage: dimg <url> [filename]"
+    exit 1
+fi
+
+URL="$1"
+FILENAME="$2"
+DEST="$HOME/Pictures/wallpapers"
+
+if ! mkdir -p "$DEST"; then
+    echo "Error: failed to create directory $DEST"
+    exit 2
+fi
+
+if ! cd "$DEST"; then
+    echo "Error: failed to access directory $DEST"
+    exit 2
+fi
+
+EXT="${URL##*.}"
+EXT="${EXT%%\?*}"
+
+if [[ -n "$FILENAME" ]]; then
+    if [[ "$FILENAME" != *.* ]]; then
+        FILENAME="$FILENAME.$EXT"
+    fi
+    if ! wget -O "$FILENAME" "$URL"; then
+        echo "Error: download failed"
+        exit 3
+    fi
+else
+    if ! wget "$URL"; then
+        echo "Error: download failed"
+        exit 3
+    fi
+fi
